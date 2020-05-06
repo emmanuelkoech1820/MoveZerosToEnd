c8b9a6ba407d5ce4e68fa5c0b2796afb904e92f5
emmanuelkoech1820 <emmanuelkoech1820@gmail.com>
2020-05-06 11:38:39 +0300

    Done

---
+++ MoveAllZerosToEndInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MoveAllZerosToEndInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9 };
+            int n = arr.Length;
+            PushZerosToEnd(arr, n);
+            Console.WriteLine("Array after pushing all zeros to the back: ");
+            for(int i = 0; i < n; i++)
+                Console.Write(arr[i] + " ");
+        }
+
+
+        static void PushZerosToEnd(int[] arr, int n)
+        {
+            int count = 0;
+            for (int i = 0; i<n; i++)
+            {
+                if (arr[i] != 0)
+                    arr[count++] = arr[i];
+            }
+            while (count < n)
+                arr[count++] = 0;
+        }
+    }
+}
